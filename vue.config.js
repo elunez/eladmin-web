233a0bc2bf18617d396f8cfc370ac892ea6e51c2
chaosx110 <recuritis@gmail.com>
2019-09-06 10:09:53 +0800

    feat(package.json and vue.config.js): 2.2DEV
    修改svg sprite

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,31 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   lintOnSave: true,
-  chainWebpack: config => {},
+  chainWebpack: config => {
+    config.module.rules.delete('svg')
+    config.module
+      .rule('svg-sprite-loader')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .end()
+    const imagesRule = config.module.rule('images')
+    imagesRule.exclude.add(resolve('src/icons'))
+    config.module.rule('images').test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
+  },
   productionSourceMap: false,
   devServer: {
     proxy: process.env.VUE_APP_BASE_API
