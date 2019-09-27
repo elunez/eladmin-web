ae71ad9731963673974220fb67469571668e123d
dqjdda <201507802@qq.com>
2019-09-27 10:29:55 +0800

    2.2版本发布，详情查阅版本说明

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"http://localhost:8000"'
-  BASE_API: '"https://api.auauz.net"'
+  BASE_API: '"http://localhost:8000"'
+  // BASE_API: '"https://api.auauz.net"'
 })
