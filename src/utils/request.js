071a3bc598f607296cfe32b61a295689afe0c1dd
dqjdda <201507802@qq.com>
2019-11-30 19:43:37 +0800

    去除缓存管理模块避免直接操作redis带来的安全隐患

--- src/utils/request.js
+++ src/utils/request.js
@@ -48,42 +48,42 @@ service.interceptors.response.use(
       if (error.toString().indexOf('Error: timeout') !== -1) {
         Notification.error({
           title: '网络请求超时',
-          duration: 2500
-        })
-        return Promise.reject(error)
-      }
-      if (error.toString().indexOf('Error: Network Error') !== -1) {
-        Notification.error({
-          title: '网络请求错误',
-          duration: 2500
+          duration: 5000
         })
         return Promise.reject(error)
       }
     }
-    if (code === 401) {
-      MessageBox.confirm(
-        '登录状态已过期，您可以继续留在该页面，或者重新登录',
-        '系统提示',
-        {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
+    if (code) {
+      if (code === 401) {
+        MessageBox.confirm(
+          '登录状态已过期，您可以继续留在该页面，或者重新登录',
+          '系统提示',
+          {
+            confirmButtonText: '重新登录',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).then(() => {
+          store.dispatch('LogOut').then(() => {
+            location.reload() // 为了重新实例化vue-router对象 避免bug
+          })
+        })
+      } else if (code === 403) {
+        router.push({ path: '/401' })
+      } else {
+        const errorMsg = error.response.data.message
+        if (errorMsg !== undefined) {
+          Notification.error({
+            title: errorMsg,
+            duration: 5000
+          })
         }
-      ).then(() => {
-        store.dispatch('LogOut').then(() => {
-          location.reload() // 为了重新实例化vue-router对象 避免bug
-        })
-      })
-    } else if (code === 403) {
-      router.push({ path: '/401' })
-    } else {
-      const errorMsg = error.response.data.message
-      if (errorMsg !== undefined) {
-        Notification.error({
-          title: errorMsg,
-          duration: 2500
-        })
       }
+    } else {
+      Notification.error({
+        title: '接口请求失败',
+        duration: 5000
+      })
     }
     return Promise.reject(error)
   }
