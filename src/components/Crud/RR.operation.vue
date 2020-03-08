c69d851ea858695505918b0f04bbda9f1d5a4fcb
Moxun1639 <mxnstrive@gmail.com>
2020-03-08 21:44:41 +0800

    1. 字典管理使用CRUD组件改写 2. CRUD组件增加对table延迟实例化（例如使用v-if）支持 3. 完善CRUD实例与组件实例注册与注销

--- src/components/Crud/RR.operation.vue
+++ src/components/Crud/RR.operation.vue
@@ -2,7 +2,7 @@
 <template>
   <span>
     <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="crud.toQuery">搜索</el-button>
-    <el-button class="filter-item" size="mini" type="warning" icon="el-icon-refresh-left" @click="crud.resetQuery()">重置</el-button>
+    <el-button v-if="crud.optShow.reset" class="filter-item" size="mini" type="warning" icon="el-icon-refresh-left" @click="crud.resetQuery()">重置</el-button>
   </span>
 </template>
 <script>
