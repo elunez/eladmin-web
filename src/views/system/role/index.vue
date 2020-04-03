60738e9f792e11455043e53792dc75b17e701d17
rikugun <v5.rikugun@gmail.com>
2020-04-03 15:47:46 +0800

    feat:    增加子级部门数据权限

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -144,7 +144,7 @@ export default {
   data() {
     return {
       defaultProps: { children: 'children', label: 'label' },
-      dateScopes: ['全部', '本级', '自定义'], level: 3,
+      dateScopes: ['全部', '本级', '子级', '自定义'], level: 3,
       currentId: 0, menuLoading: false, showButton: false,
       menus: [], menuIds: [], depts: [],
       permission: {
