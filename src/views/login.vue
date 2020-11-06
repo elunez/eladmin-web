b0483150ea27d83e61b16db5e40ce140760cb091
ZhengJie <201507802@qq.com>
2020-11-06 22:47:16 +0800

    [代码优化](v2.6)：update

--- src/views/login.vue
+++ src/views/login.vue
@@ -36,7 +36,7 @@
     <div v-if="$store.state.settings.showFooter" id="el-login-footer">
       <span v-html="$store.state.settings.footerTxt" />
       <span> ⋅ </span>
-      <a href="http://www.beian.miit.gov.cn" target="_blank">{{ $store.state.settings.caseNumber }}</a>
+      <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">{{ $store.state.settings.caseNumber }}</a>
     </div>
   </div>
 </template>
