9813894c8f8ebab3c84b9038a9343f25ad9c823b
zhengjie <201507802@qq.com>
2020-12-19 18:07:02 +0800

    [代码优化](v2.6)：支持三级菜单的页面缓存

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -9,7 +9,7 @@
     <div>
       <blockquote class="my-blockquote"> 三级菜单缓存配置教程</blockquote>
       <pre class="my-code">
- 1、将前后端代码更新为最新版版本，或者对照提交记录进行修改
+ 1、将前后端代码更新为最新版版本，或对照提交记录修改,点击查看-> <a href="https://gitee.com/elunez/eladmin/commit/43d1a63577f9d5347924355708429a2d210e29f7" target="_blank">提交(1)</a>、<a href="https://gitee.com/elunez/eladmin/commit/46393875148fcca5eaa327d4073f72edb3752f5c" target="_blank">提交(2)</a>、<a href="https://gitee.com/elunez/eladmin-web/commit/c93c99d8921abbb2c52afc806635f5ca08d6bda8" target="_blank">提交(3)</a>
  2、将 二级菜单 的 菜单类型 设置为 目录 级别，并且原有的 组件路径 需要清空
  3、将 三级菜单 的 菜单缓存 设置为 是，最后将 组件名称 填写正确
  4、具体设置可参考 菜单管理 的 多级菜单 配置进行进行相应的修改
@@ -29,3 +29,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .my-code a{
+    color:#009688;
+  }
+</style>
