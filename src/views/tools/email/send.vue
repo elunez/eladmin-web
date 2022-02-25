46f5d401c66125c1ba0d05b220b59ac84473f3e7
Zheng Jie <201507802@qq.com>
2022-02-25 10:50:49 +0800

    修复富文本版本升级带来的问题

--- src/views/tools/email/send.vue
+++ src/views/tools/email/send.vue
@@ -2,19 +2,13 @@
   <div>
     <el-form ref="form" :model="form" :rules="rules" style="margin-top: 6px;" size="small" label-width="100px">
       <el-form-item label="邮件标题" prop="subject">
-        <el-input v-model="form.subject" style="width: 646px" />
+        <el-input v-model="form.subject" style="width: 646px" placeholder="请输入邮件标题，标题不能为空" />
       </el-form-item>
-      <el-form-item
-        v-for="(domain, index) in tos"
-        :key="domain.key"
-        :label="'收件邮箱' + (index === 0 ? '': index)"
-      >
-        <el-input v-model="domain.value" style="width: 550px" />
-        <el-button icon="el-icon-plus" @click="addDomain" />
-        <el-button style="margin-left:0;" icon="el-icon-minus" @click.prevent="removeDomain(domain)" />
+      <el-form-item label="收件地址" prop="tos">
+        <el-input v-model="form.tos" style="width: 646px" placeholder="请输入收件地址，多个地址英文逗号,隔开" />
       </el-form-item>
       <div ref="editor" class="editor" />
-      <el-button :loading="loading" style="margin-left:1.6%;" size="medium" type="primary" @click="doSubmit">发送邮件</el-button>
+      <el-button :loading="loading" style="margin-left:1.6%;margin-bottom: 30px" size="medium" type="primary" @click="doSubmit">发送邮件</el-button>
     </el-form>
   </div>
 </template>
@@ -22,93 +16,55 @@
 <script>
 import { send } from '@/api/tools/email'
 import { upload } from '@/utils/upload'
-import { validEmail } from '@/utils/validate'
 import { mapGetters } from 'vuex'
 import E from 'wangeditor'
 export default {
   name: 'Index',
   data() {
     return {
-      loading: false, form: { subject: '', tos: [], content: '' },
-      tos: [{
-        value: ''
-      }],
+      loading: false, form: { subject: '', tos: '', content: '' },
       rules: {
         subject: [
           { required: true, message: '标题不能为空', trigger: 'blur' }
+        ],
+        tos: [
+          { required: true, message: '收件人不能为空', trigger: 'blur' }
         ]
       }
     }
   },
   computed: {
     ...mapGetters([
-      'imagesUploadApi'
+      'imagesUploadApi',
+      'baseApi'
     ])
   },
   mounted() {
     const _this = this
     var editor = new E(this.$refs.editor)
     // 自定义菜单配置
-    editor.customConfig.zIndex = 10
+    editor.config.zIndex = 10
     // 文件上传
-    editor.customConfig.customUploadImg = function(files, insert) {
+    editor.config.customUploadImg = function(files, insert) {
       // files 是 input 中选中的文件列表
       // insert 是获取图片 url 后，插入到编辑器的方法
       files.forEach(image => {
-        files.forEach(image => {
-          upload(_this.imagesUploadApi, image).then(data => {
-            insert(data.data.url)
-          })
+        upload(_this.imagesUploadApi, image).then(res => {
+          const data = res.data
+          const url = _this.baseApi + '/file/' + data.type + '/' + data.realName
+          insert(url)
         })
       })
     }
-    editor.customConfig.onchange = (html) => {
+    editor.config.onchange = (html) => {
       this.form.content = html
     }
     editor.create()
   },
   methods: {
-    removeDomain(item) {
-      var index = this.tos.indexOf(item)
-      if (index !== -1 && this.tos.length !== 1) {
-        this.tos.splice(index, 1)
-      } else {
-        this.$message({
-          message: '请至少保留一位联系人',
-          type: 'warning'
-        })
-      }
-    },
-    addDomain() {
-      this.tos.push({
-        value: '',
-        key: Date.now()
-      })
-    },
     doSubmit() {
-      const _this = this
       this.$refs['form'].validate((valid) => {
-        this.form.tos = []
         if (valid) {
-          let sub = false
-          this.tos.forEach(function(data, index) {
-            if (data.value === '') {
-              _this.$message({
-                message: '收件邮箱不能为空',
-                type: 'warning'
-              })
-              sub = true
-            } else if (validEmail(data.value)) {
-              _this.form.tos.push(data.value)
-            } else {
-              _this.$message({
-                message: '收件邮箱格式错误',
-                type: 'warning'
-              })
-              sub = true
-            }
-          })
-          if (sub) { return false }
           this.loading = true
           send(this.form).then(res => {
             this.$notify({
