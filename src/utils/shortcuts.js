6c01b15ed28cd128804274b18e78d0301e3781ab
Zheng Jie <201507802@qq.com>
2022-05-18 13:51:42 +0800

    日期控件优化

--- src/utils/shortcuts.js
+++ src/utils/shortcuts.js
@@ -15,16 +15,9 @@ export const calendarBaseShortcuts = [{
     picker.$emit('pick', [startTime, endTime])
   }
 }, {
-  text: '最近一周',
+  text: '本周',
   onClick(picker) {
-    const startTime = new Date(new Date().daysAgo(7).setHours(0, 0, 0))
-    const endTime = new Date(new Date().setHours(23, 59, 59))
-    picker.$emit('pick', [startTime, endTime])
-  }
-}, {
-  text: '最近30天',
-  onClick(picker) {
-    const startTime = new Date(new Date().daysAgo(30).setHours(0, 0, 0))
+    const startTime = new Date(new Date().daysAgo(new Date().getDay() - 1).setHours(0, 0, 0))
     const endTime = new Date(new Date().setHours(23, 59, 59))
     picker.$emit('pick', [startTime, endTime])
   }
@@ -36,9 +29,16 @@ export const calendarBaseShortcuts = [{
     picker.$emit('pick', [startTime, endTime])
   }
 }, {
-  text: '本季度',
+  text: '当前季度',
   onClick(picker) {
     const startTime = new Date(new Date().quarterBegin().setHours(0, 0, 0))
+    const endTime = new Date(new Date().setHours(23, 59, 59))
+    picker.$emit('pick', [startTime, endTime])
+  }
+}, {
+  text: '最近一个月',
+  onClick(picker) {
+    const startTime = new Date(new Date().daysAgo(30).setHours(0, 0, 0))
     const endTime = new Date(new Date().setHours(23, 59, 59))
     picker.$emit('pick', [startTime, endTime])
   }
@@ -66,38 +66,6 @@ export const calendarMoveShortcuts = [{
     }
     startTime = picker.value[0].daysAgo(-1)
     endTime = picker.value[1].daysAgo(-1)
-    picker.$emit('pick', [startTime, endTime])
-  }
-}, {
-  text: '« 往前一周 ',
-  onClick(picker) {
-    let startTime = new Date(new Date().setHours(0, 0, 0))
-    let endTime = new Date(new Date().setHours(23, 59, 59))
-    if (!picker.value) {
-      picker.value = [startTime.daysAgo(new Date().getDay()),
-        endTime.daysAgo(new Date().getDay() + 1)]
-    } else {
-      picker.value = [picker.value[0].daysAgo(picker.value[0].getDay()),
-        picker.value[1].daysAgo(picker.value[1].getDay() + 1)]
-    }
-    startTime = picker.value[0].daysAgo(7)
-    endTime = picker.value[1]
-    picker.$emit('pick', [startTime, endTime])
-  }
-}, {
-  text: ' 往后一周 »',
-  onClick(picker) {
-    let startTime = new Date(new Date().setHours(0, 0, 0))
-    let endTime = new Date(new Date().setHours(23, 59, 59))
-    if (!picker.value) {
-      picker.value = [startTime.daysAgo(new Date().getDay() - 7),
-        endTime.daysAgo(new Date().getDay() - 6)]
-    } else {
-      picker.value = [picker.value[0].daysAgo(picker.value[0].getDay() - 7),
-        picker.value[1].daysAgo(picker.value[1].getDay() - 6)]
-    }
-    startTime = picker.value[0]
-    endTime = picker.value[1].daysAgo(-7)
     picker.$emit('pick', [startTime, endTime])
   }
 }]
