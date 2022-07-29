5cc21ac695b4d19d596e1b8f1ded00f0a9179b12
Zheng Jie <201507802@qq.com>
2022-07-29 18:07:08 +0800

    update

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -6,6 +6,7 @@
       </keep-alive>
     </transition>
     <div
+      :key="key"
       class="wwads-cn wwads-vertical"
       data-id="148"
       style="z-index:10000;position:fixed;bottom:33px;right:5px;max-width:200px;padding:10px;border-radius:5px;"
@@ -34,13 +35,6 @@ export default {
       return this.$route.path
     }
   }
-  // watch: {
-  //   '$route.path': {
-  //     handler: function() {
-  //       this.key = this.$route.path
-  //     }
-  //   }
-  // }
 }
 </script>
 
@@ -77,6 +71,6 @@ export default {
   }
 }
 .wwads-vertical .wwads-poweredby {
-  bottom: 2px !important;
+  display: none;
 }
 </style>
