de479a837bc0fbf785a43621909fe4ee6be43c39
Zheng Jie <201507802@qq.com>
2022-08-05 17:50:40 +0800

    update

--- src/components/Ad/index.vue
+++ src/components/Ad/index.vue
@@ -1,12 +1,11 @@
 <template>
-  <div id="ww-container">
-    <div
-      v-if="show"
-      class="wwads-cn wwads-vertical"
-      data-id="148"
-      style="z-index:10000;position:fixed;bottom:33px;right:1px;max-width:200px;padding: 12px 10px;border-radius:5px;"
-    />
-  </div>
+  <div
+    v-if="show"
+    id="ww-container"
+    class="wwads-cn wwads-vertical"
+    data-id="148"
+    style="z-index:10000;position:fixed;bottom:33px;right:1px;max-width:200px;padding: 12px 10px;border-radius:5px;"
+  />
 </template>
 
 <script>
