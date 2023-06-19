63007a02147970ed04a4c15b58f91d30efa311b6
Zheng Jie <201507802@qq.com>
2023-06-19 16:12:22 +0800

    修复运维管理服务器查询问题

--- src/views/mnt/server/index.vue
+++ src/views/mnt/server/index.vue
@@ -4,7 +4,7 @@
     <div class="head-container">
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
-        <el-input v-model="query.id" clearable placeholder="输入名称或IP搜索" style="width: 200px" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <el-input v-model="query.blurry" clearable placeholder="输入名称或IP搜索" style="width: 200px" class="filter-item" @keyup.enter.native="crud.toQuery" />
         <date-range-picker v-model="query.createTime" class="date-item" />
         <rrOperation />
       </div>
